4f9a267b98217346ec715af946685b5c226da302
Fayland Lam <fayland@gmail.com>
2011-11-27 16:56:34 +0800

    SQL fixes

--- Web/KinderGarden/Web.pm
+++ Web/KinderGarden/Web.pm
@@ -27,7 +27,7 @@ get '/auth' => sub {
         my ($user_id) = $dbh->selectrow_array("SELECT user_id FROM user_auth WHERE type_id = ? AND identification = ?", undef, $user_auth_type{$auth_provider}, $user->{id});
 
         if ($user_id) {
-            $dbh->do("UPDATE user SET visited_at = ? WHERE user_id = ?", undef, time(), $user_id);
+            $dbh->do("UPDATE user SET visited_at = ? WHERE id = ?", undef, time(), $user_id);
             $dbh->do("UPDATE user_auth SET raw_data = ? WHERE type_id = ? AND identification = ?", undef, to_json($user), $user_auth_type{$auth_provider}, $user->{id});
         } else {
             $dbh->do("INSERT INTO user (name, email, signed_at, visited_at) VALUES (?, ?, ?, ?)", undef, $user->{name}, $user->{email}, time(), time());
