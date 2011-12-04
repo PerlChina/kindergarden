0f025d501d51d8b9c4fa573def9ea2cc65af1614
Fayland Lam <fayland@gmail.com>
2011-12-04 11:09:28 +0800

    using own Dancer::Teamplte::Xslate

--- bin/install/install.sh
+++ bin/install/install.sh
@@ -6,13 +6,14 @@ cpanm Plack;
 cpanm Text::Xslate;
 cpanm Moose;
 cpanm Dancer;
-cpanm Dancer::Template::Xslate;
+cpanm http://fayland.org/CPAN/Dancer-Template-Xslate-0.01.tar.gz
 cpanm Dancer::Session::PSGI;
-cpanm Dancer::Logger::PSGI;
+cpanm -f Dancer::Logger::PSGI;
 cpanm Mojolicious ;
 cpanm MojoX::Renderer::Xslate;
 cpanm Plack::Middleware::Session;
 cpanm CHI;
 cpanm Cache::FastMmap;
 cpanm Plack::Middleware::OAuth;
+cpanm Plack::Middleware::Debug;
 cpanm Gravatar::URL;
